1d81e5cbd68db54e96b022f7833407f21431feeb
zhchy <>
2019-10-08 15:13:54 +0800

    更新二叉树

---
+++ dahua/5/5-1.c
@@ -0,0 +1,15 @@
+#include "head.c"
+
+typedef struct TNode {
+  ElemType data;
+  struct TNode *lChild, *rChild;
+} TNode, *Tree;
+
+Status CreateTree(Tree *T, char[]) {
+  
+  
+}
+
+int main() {
+  return 0;
+}
